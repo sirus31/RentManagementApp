b43c7ab31cabe1d48f4b78a8c6b36ea47c5bd100
Sirus <sirussahofficial@gmail.com>
2026-06-16 23:20:31 +0530

    chore: update cors configuration for frontend hosting
    - allow deployed frontend access to backend API
    - prepare backend for production integration

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
         policy =>
         {
             policy
-                .WithOrigins("http://localhost:5173")
+                .AllowAnyOrigin()
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
